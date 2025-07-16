7e466268fe05e07c8b2cb4eafa6f8673475f01d6
Lakshya Gupta <lakshya.gupta@adglobal360.in>
2025-07-16 21:48:47 +0530

    created the forgot password api

--- src/controllers/userControllers/forgotPassword.controller.js
+++ src/controllers/userControllers/forgotPassword.controller.js
@@ -3,28 +3,31 @@ import {
   successResponse,
 } from "../../utils/apiResponse.utils.js";
 import { User } from "../../models/user.model.js";
+import { geenrateResetToken } from "../../utils/generateTokens.utils.js";
 
 // yet to be implemented
 
 const forgotPasswordController = async (req, res) => {
   const { email } = req.body;
-  // 1. get the email from body
-  // 2. check if email is not empty
-  // 3. if not empty, check if the user with this email exists or not
-  // 4. if exists,
+
   try {
-    // check if email is not empty
     if (!email) return errorResponse(res, 400, "Email is required...");
 
-    // check if user with the same email id exists or not
     const user = await User.findOne({ email });
     if (!user) return errorResponse(res, 404, "User not found...");
 
-    res
-      .status(200)
-      .json({ message: "Mail sent successfully to your registered email." });
+    const resetTokenData = {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    };
+    const resetToken = geenrateResetToken(resetTokenData);
+
+    const resetLink = `${process.env.BASE_URL}/resetPassword?token=${resetToken}`;
+
+    successResponse(res, 'Mail sent successfully to your registered email.', resetLink)
   } catch (error) {
-    res.status(400).json({ message: "Error sending reset email" });
+    return errorResponse(res, 'Error sending email to the registered email id')
   }
 };
 
--- src/utils/generateTokens.utils.js
+++ src/utils/generateTokens.utils.js
@@ -2,12 +2,18 @@ import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (data) => {
   return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn:  process.env.ACCESS_TOKEN_EXPIRY,
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
   });
 };
 
 export const generateRefreshToken = (data) => {
-  return jwt.sign(data,  process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn:  process.env.REFRESH_TOKEN_EXPIRY,
+  return jwt.sign(data, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
   });
 };
+
+export const geenrateResetToken = (data) => {
+  return jwt.sign(data, process.env.RESET_TOKEN_SECRET, {
+    expiresIn: process.env.RESET_TOKEN_EXPIRY,
+  })
+}
